cdfd1afca7faf6b1f1dc4d8c3866ad8e1d73548b
mau <geraldezmau@gmail.com>
2017-04-13 18:17:25 +0800

    added javascript for login.html

---
+++ js/login.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+		
+		function check(form) {
+			if (form.username.value == "attendancechecker" && form.pass.value == "achecker2017") {
+				window.open("index.html")
+			} else {
+			alert("Username or password incorrect.")
+			}
+			
+		}
+</script>
