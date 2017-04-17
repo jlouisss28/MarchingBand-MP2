2bccf9c8ad82e87d7092d2fb9effc2534a00c109
RenzSiapno <2152589@slu.edu.ph>
2017-04-17 09:07:47 +0800

    .

--- css/xmlhttprequest.js
+++ css/xmlhttprequest.js
@@ -1,15 +1,4 @@
-	for(var i=0; i<400;i++) {
-		var request = new XMLHttpRequest();
-	request.open('GET','Members.txt', true);
+	var request = new XMLHttpRequest();
+	request.open('GET','members.txt', false);
 	request.send();
-
-	request.onreadystatechange = function() {
-		if(request.readyState<4) {
-			document.getElementById('div1').innerHTML = "Loading...";
-			console.log(request);
-		} else if (request.readyState===4 && request.status===200) {
-			document.getElementById('div1').innerHTML = request.responseText;
-			console.log(request);
-		}
-	}
-	}
+	console.log(request);
