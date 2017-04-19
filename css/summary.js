0c6510feb4fc6142bf992de2958a0d335cd37437
marinelleAlegre <biku.tomoshimasen@gmail.com>
2017-04-19 21:47:25 +0800

    Continue adding js for attendance and style for reports.html table

--- css/summary.js
+++ css/summary.js
@@ -1,286 +1,566 @@
+var today = new Date(); 
+
 //absences
-function saveFlute1Mem1Abs() {
-    var today = new Date();	
-	localStorage.setItem('Flute 1 Member 1 Absent ', today);
+function saveFlute1Mem1Abs() {	
+	localStorage.setItem('Flute 1 Member 1 Absent', today);
 }
 
 function saveFlute1Mem2Abs() {
-    var today = new Date();	
 	localStorage.setItem('Flute 1 Member 2 Absent ', today);
 }
 
 function saveFlute1Mem3Abs() {
-    var today = new Date();	
 	localStorage.setItem('Flute 1 Member 3 Absent ', today);
 }
 
 function saveFlute1Mem4Abs() {
-    var today = new Date();	
 	localStorage.setItem('Flute 1 Member 4 Absent ', today);
 }
 
 function saveFlute1Mem5Abs() {
-    var today = new Date();	
 	localStorage.setItem('Flute 1 Member 5 Absent ', today);
 }
 
 function saveFlute1Mem6Abs() {
-    var today = new Date();	
 	localStorage.setItem('Flute 1 Member 6 Absent ', today);
 }
 
 function saveFlute1Mem7Abs() {
-    var today = new Date();	
 	localStorage.setItem('Flute 1 Member 7 Absent ', today);
 }
 
 function saveFlute1Mem8Abs() {
-    var today = new Date();	
 	localStorage.setItem('Flute 1 Member 8 Absent ', today);
 }
 
 function saveFlute1Mem9Abs() {
-    var today = new Date();	
 	localStorage.setItem('Flute 1 Member 9 Absent ', today);
 }
 
 function saveFlute1Mem10Abs() {
-    var today = new Date();	
 	localStorage.setItem('Flute 1 Member 10 Absent ', today);
 }
 
 function saveFlute2Mem1Abs() {
-    var today = new Date();	
 	localStorage.setItem('Flute 2 Member 1 Absent ', today);
 }
 
 function saveFlute2Mem2Abs() {
-    var today = new Date();	
 	localStorage.setItem('Flute 2 Member 2 Absent ', today);
 }
 
 function saveFlute2Mem3Abs() {
-    var today = new Date();	
 	localStorage.setItem('Flute 2 Member 3 Absent ', today);
 }
 
 function saveFlute3Mem4Abs() {
-    var today = new Date();	
 	localStorage.setItem('Flute 2 Member 4 Absent ', today);
 }
 
 function saveFlute2Mem5Abs() {
-    var today = new Date();	
 	localStorage.setItem('Flute 2 Member 5 Absent ', today);
 }
 
 function saveFlute2Mem6Abs() {
-    var today = new Date();	
 	localStorage.setItem('Flute 2 Member 6 Absent ', today);
 }
 
 function saveFlute2Mem7Abs() {
-    var today = new Date();	
 	localStorage.setItem('Flute 2 Member 7 Absent ', today);
 }
 
 function saveFlute2Mem8Abs() {
-    var today = new Date();	
 	localStorage.setItem('Flute 2 Member 8 Absent ', today);
 }
 
 function saveFlute2Mem9Abs() {
-    var today = new Date();	
 	localStorage.setItem('Flute 2 Member 9 Absent ', today);
 }
 
 function saveFlute2Mem10Abs() {
-    var today = new Date();	
 	localStorage.setItem('Flute 2 Member 10 Absent ', today);
 }
 
-function saveClar1Mem1Abs() {
-    var today = new Date();	
+function saveClar1Mem1Abs() {	
 	localStorage.setItem('Clarinet 1 Member 1 Absent ', today);
 }
+
 function saveClar1Mem2Abs() {
-    var today = new Date();	
 	localStorage.setItem('Clarinet 1 Member 2 Absent ', today);
 }
-function saveClar1Mem3Abs() {
-    var today = new Date();	
+
+function saveClar1Mem3Abs() {	
 	localStorage.setItem('Clarinet 1 Member 3 Absent ', today);
 }
+
 function saveClar1Mem4Abs() {
-    var today = new Date();	
 	localStorage.setItem('Clarinet 1 Member 4 Absent ', today);
 }
-function saveClar1Mem5Abs() {
-    var today = new Date();	
+
+function saveClar1Mem5Abs() {	
 	localStorage.setItem('Clarinet 1 Member 5 Absent ', today);
 }
+
 function saveClar1Mem6Abs() {
-    var today = new Date();	
 	localStorage.setItem('Clarinet 1 Member 6 Absent ', today);
 }
+
 function saveClar1Mem7Abs() {
-    var today = new Date();	
 	localStorage.setItem('Clarinet 1 Member 7 Absent ', today);
 }
-function saveClar1Mem8Abs() {
-    var today = new Date();	
+
+function saveClar1Mem8Abs() {	
 	localStorage.setItem('Clarinet 1 Member 8 Absent ', today);
 }
+
 function saveClar1Mem9Abs() {
-    var today = new Date();	
 	localStorage.setItem('Clarinet 1 Member 9 Absent ', today);
 }
-function saveClar1Mem10Abs() {
-    var today = new Date();	
+
+function saveClar1Mem10Abs() {	
 	localStorage.setItem('Clarinet 1 Member 10 Absent ', today);
 }
 
+function saveClar2Mem1Abs() {
+    localStorage.setItem('Clarinet 2 Member 1 Absent ', today);
+}
+
+function saveClar2Mem2Abs() {
+    localStorage.setItem('Clarinet 2 Member 2 Absent ', today);
+}
+
+function saveClar2Mem3Abs() {
+    localStorage.setItem('Clarinet 2 Member 3 Absent ', today);
+}
+
+function saveClar2Mem4Abs() {
+    localStorage.setItem('Clarinet 2 Member 4 Absent ', today);
+}
+
+function saveClar2Mem5Abs() {
+    localStorage.setItem('Clarinet 2 Member 5 Absent ', today);
+}
+
+function saveClar2Mem6Abs() {
+    localStorage.setItem('Clarinet 2 Member 6 Absent ', today);
+}
+
+function saveClar2Mem7Abs() { 
+    localStorage.setItem('Clarinet 2 Member 7 Absent ', today);
+}
+
+function saveClar2Mem8Abs() {
+    localStorage.setItem('Clarinet 2 Member 8 Absent ', today);
+}
+
+function saveClar2Mem9Abs() {
+    localStorage.setItem('Clarinet 2 Member 9 Absent ', today);
+}
+
+function saveClar2Mem10Abs() {
+    localStorage.setItem('Clarinet 2 Member 10 Absent ', today);
+}
+
+function saveTrum1Mem1Abs() {
+    localStorage.setItem('Trumpet 1 Member 1 Absent ', today);
+}
+
+function saveTrum1Mem21Abs() {
+    localStorage.setItem('Trumpet 1 Member 2 Absent ', today);
+}
+
+function saveTrum1Mem3Abs() {
+    localStorage.setItem('Trumpet 1 Member 3 Absent ', today);
+}
+
+function saveTrum1Mem4Abs() {
+    localStorage.setItem('Trumpet 1 Member 4 Absent ', today);
+}
+
+function saveTrum1Mem5Abs() {
+    localStorage.setItem('Trumpet 1 Member 5 Absent ', today);
+}
+
+function saveTrum1Mem6Abs() {
+    localStorage.setItem('Trumpet 1 Member 6 Absent ', today);
+}
+
+function saveTrum1Mem7Abs() {
+    localStorage.setItem('Trumpet 1 Member 7 Absent ', today);
+}
+
+function saveTrum1Mem8Abs() {
+    localStorage.setItem('Trumpet 1 Member 8 Absent ', today);
+}
+
+function saveTrum1Mem9Abs() {
+    localStorage.setItem('Trumpet 1 Member 9 Absent ', today);
+}
+
+function saveTrum1Mem10Abs() {
+    localStorage.setItem('Trumpet 1 Member 10 Absent ', today);
+}
+
+function saveTrum2Mem1Abs() {
+    localStorage.setItem('Trumpet 2 Member 1 Absent ', today);
+}
+
+function saveTrum2Mem2Abs() {
+    localStorage.setItem('Trumpet 2 Member 2 Absent ', today);
+}
+
+function saveTrum2Mem3Abs() {
+    localStorage.setItem('Trumpet 2 Member 3 Absent ', today);
+}
+
+function saveTrum2Mem4Abs() {
+    localStorage.setItem('Trumpet 2 Member 4 Absent ', today);
+}
+
+function saveTrum2Mem5Abs() {
+    localStorage.setItem('Trumpet 2 Member 5 Absent ', today);
+}
+
+function saveTrum2Mem6Abs() {
+    localStorage.setItem('Trumpet 2 Member 6 Absent ', today);
+}
+
+function saveTrum2Mem7Abs() {
+    localStorage.setItem('Trumpet 2 Member 7 Absent ', today);
+}
+
+function saveTrum2Mem8Abs() {
+    localStorage.setItem('Trumpet 2 Member 8 Absent ', today);
+}
+
+function saveTrum2Mem9Abs() {
+    localStorage.setItem('Trumpet 2 Member 9 Absent ', today);
+}
+
+function saveTrum2Mem10Abs() {
+    localStorage.setItem('Trumpet 1 Member 10 Absent ', today);
+}
+
+function saveTrum3Mem1Abs() {
+    localStorage.setItem('Trumpet 3 Member 1 Absent ', today);
+}
+
+function saveTrum3Mem2Abs() {
+    localStorage.setItem('Trumpet 3 Member 2 Absent ', today);
+}
+
+function saveTrum3Mem3Abs() {
+    localStorage.setItem('Trumpet 3 Member 3 Absent ', today);
+}
+
+function saveTrum3Mem4Abs() {
+    localStorage.setItem('Trumpet 3 Member 4 Absent ', today);
+}
+
+function saveTrum3Mem5Abs() {
+    localStorage.setItem('Trumpet 3 Member 5 Absent ', today);
+}
+
+function saveTrum3Mem6Abs() {
+    localStorage.setItem('Trumpet 3 Member 6 Absent ', today);
+}
+
+function saveTrum3Mem7Abs() {
+    localStorage.setItem('Trumpet 3 Member 7 Absent ', today);
+}
+
+function saveTrum3Mem8Abs() {
+    localStorage.setItem('Trumpet 3 Member 8 Absent ', today);
+}
+
+function saveTrum3Mem9Abs() {
+    localStorage.setItem('Trumpet 3 Member 9 Absent ', today);
+}
+
+function saveTrum3Mem10Abs() {
+    localStorage.setItem('Trumpet 3 Member 10 Absent ', today);
+}
+
+
 //late
 function saveFlute1Mem1Late() {
-    var today = new Date();	
 	localStorage.setItem('Flute 1 Member 1 Late ', today);
 }
 
 function saveFlute1Mem2Late() {
-    var today = new Date();	
 	localStorage.setItem('Flute 1 Member 2 Late ', today);
 }
 
 function saveFlute1Mem3Late() {
-    var today = new Date();	
 	localStorage.setItem('Flute 1 Member 3 Late ', today);
 }
 
 function saveFlute1Mem4Late() {
-    var today = new Date();	
 	localStorage.setItem('Flute 1 Member 4 Late ', today);
 }
 
 function saveFlute1Mem5Late() {
-    var today = new Date();	
 	localStorage.setItem('Flute 1 Member 5 Late ', today);
 }
 
 function saveFlute1Mem6Late() {
-    var today = new Date();	
 	localStorage.setItem('Flute 1 Member 6 Late ', today);
 }
 
 function saveFlute1Mem7Late() {
-    var today = new Date();	
 	localStorage.setItem('Flute 1 Member 7 Late ', today);
 }
 
 function saveFlute1Mem8Late() {
-    var today = new Date();	
 	localStorage.setItem('Flute 1 Member 8 Late ', today);
 }
 
 function saveFlute1Mem9Late() {
-    var today = new Date();	
 	localStorage.setItem('Flute 1 Member 9 Late ', today);
 }
 
 function saveFlute1Mem10Late() {
-    var today = new Date();	
 	localStorage.setItem('Flute 1 Member 10 Late ', today);
 }
 
 function saveFlute2Mem1Late() {
-    var today = new Date();	
 	localStorage.setItem('Flute 2 Member 1 Late ', today);
 }
 
 function saveFlute2Mem2Late() {
-    var today = new Date();	
 	localStorage.setItem('Flute 2 Member 2 Late ', today);
 }
 
 function saveFlute2Mem3Late() {
-    var today = new Date();	
 	localStorage.setItem('Flute 2 Member 3 Late ', today);
 }
 
 function saveFlute2Mem4Late() {
-    var today = new Date();	
 	localStorage.setItem('Flute 2 Member 4 Late ', today);
 }
 
 function saveFlute2Mem5Late() {
-    var today = new Date();	
 	localStorage.setItem('Flute 2 Member 5 Late ', today);
 }
 
 function saveFlute2Mem6Late() {
-    var today = new Date();	
 	localStorage.setItem('Flute 2 Member 6 Late ', today);
 }
 
 function saveFlute2Mem7Late() {
-    var today = new Date();	
 	localStorage.setItem('Flute 2 Member 7 Late ', today);
 }
 
 function saveFlute2Mem8Late() {
-    var today = new Date();	
 	localStorage.setItem('Flute 2 Member 8 Late ', today);
 }
 
 function saveFlute2Mem9ate() {
-    var today = new Date();	
 	localStorage.setItem('Flute 2 Member 9 Late ', today);
 }
 
 function saveFlute2Mem10Late() {
-    var today = new Date();	
 	localStorage.setItem('Flute 2 Member 10 Late ', today);
 }
 
 function saveClar1Mem1Late() {
-    var today = new Date();	
 	localStorage.setItem('Clarinet 1 Member 1 Late ', today);
 }
 
 function saveClar1Mem2Late() {
-    var today = new Date();	
 	localStorage.setItem('Clarinet 1 Member 2 Late ', today);
 }
-function saveClar1Mem3Late() {
-    var today = new Date();	
+
+function saveClar1Mem3Late() {	
 	localStorage.setItem('Clarinet 1 Member 3 Late ', today);
 }
+
 function saveClar1Mem4Late() {
-    var today = new Date();	
 	localStorage.setItem('Clarinet 1 Member 4 Late ', today);
 }
+
 function saveClar1Mem5Late() {
-    var today = new Date();	
 	localStorage.setItem('Clarinet 1 Member 5 Late ', today);
 }
+
 function saveClar1Mem6Late() {
-    var today = new Date();	
 	localStorage.setItem('Clarinet 1 Member 6 Late ', today);
 }
+
 function saveClar1Mem7Late() {
-    var today = new Date();	
 	localStorage.setItem('Clarinet 1 Member 7 Late ', today);
 }
+
 function saveClar1Mem8Late() {
-    var today = new Date();	
 	localStorage.setItem('Clarinet 1 Member 8 Late ', today);
 }
+
 function saveClar1Mem9Late() {
-    var today = new Date();	
 	localStorage.setItem('Clarinet 1 Member 9 Late ', today);
 }
+
 function saveClar1Mem10Late() {
-    var today = new Date();	
 	localStorage.setItem('Clarinet 1 Member 10 Late ', today);
+}
+
+function saveClar2Mem1Late() {
+    localStorage.setItem('Clarinet 2 Member 1 Late ', today);
+}
+
+function saveClar2Mem2Late() {
+    localStorage.setItem('Clarinet 2 Member 2 Late ', today);
+}
+
+function saveClar2Mem3Late() {
+    localStorage.setItem('Clarinet 2 Member 3 Late ', today);
+}
+
+function saveClar2Mem4Late() { 
+    localStorage.setItem('Clarinet 2 Member 4 Late ', today);
+}
+
+function saveClar2Mem5Late() {
+    localStorage.setItem('Clarinet 2 Member 5 Late ', today);
+}
+
+function saveClar2Mem6Late() {
+    localStorage.setItem('Clarinet 2 Member 6 Late ', today);
+}
+
+function saveClar2Mem7Late() {
+    localStorage.setItem('Clarinet 2 Member 7 Late ', today);
+}
+
+function saveClar2Mem8Late() {
+    localStorage.setItem('Clarinet 2 Member 8 Late ', today);
+}
+
+function saveClar2Mem9Late() {
+    localStorage.setItem('Clarinet 2 Member 9 Late ', today);
+}
+
+function saveClar2Mem10Late() {
+    localStorage.setItem('Clarinet 2 Member 10 Late ', today);
+}
+
+function saveTrum1Mem1Late() {
+    localStorage.setItem('Trumpet 1 Member 1 Late ', today);
+}
+
+function saveTrum1Mem2Late() {
+    localStorage.setItem('Trumpet 1 Member 2 Late ', today);
+}
+
+function saveTrum1Mem3Late() {
+    localStorage.setItem('Trumpet 1 Member 3 Late ', today);
+}
+
+function saveTrum1Mem4Late() { 
+    localStorage.setItem('Trumpet 1 Member 4 Late ', today);
+}
+
+function saveTrum1Mem5Late() {
+    localStorage.setItem('Trumpet 1 Member 5 Late ', today);
+}
+
+function saveTrum1Mem6Late() {
+    localStorage.setItem('Trumpet 1 Member 6 Late ', today);
+}
+
+function saveTrum1Mem7Late() {
+    localStorage.setItem('Trumpet 1 Member 7 Late ', today);
+}
+
+function saveTrum1Mem8Late() {
+    localStorage.setItem('Trumpet 1 Member 8 Late ', today);
+}
+
+function saveTrum1Mem9Late() {
+    localStorage.setItem('Trumpet 1 Member 9 Late ', today);
+}
+
+function saveTrum1Mem10Late() {
+    localStorage.setItem('Trumpet 1 Member 10 Late ', today);
+}
+
+function saveTrum2Mem1Late() {
+    localStorage.setItem('Trumpet 2 Member 1 Late ', today);
+}
+
+function saveTrum2Mem2Late() {
+    localStorage.setItem('Trumpet 2 Member 2 Late ', today);
+}
+
+function saveTrum2Mem3Late() {
+    localStorage.setItem('Trumpet 2 Member 3 Late ', today);
+}
+
+function saveTrum2Mem4Late() { 
+    localStorage.setItem('Trumpet 2 Member 4 Late ', today);
+}
+
+function saveTrum2Mem5Late() {
+    localStorage.setItem('Trumpet 2 Member 5 Late ', today);
+}
+
+function saveTrum2Mem6Late() {
+    localStorage.setItem('Trumpet 2 Member 6 Late ', today);
+}
+
+function saveTrum2Mem7Late() {
+    localStorage.setItem('Trumpet 2 Member 7 Late ', today);
+}
+
+function saveTrum2Mem8Late() {
+    localStorage.setItem('Trumpet 2 Member 8 Late ', today);
+}
+
+function saveTrum2Mem9Late() {
+    localStorage.setItem('Trumpet 2 Member 9 Late ', today);
+}
+
+function saveTrum2Mem10Late() {
+    localStorage.setItem('Trumpet 2 Member 10 Late ', today);
+}
+
+function saveTrum3Mem1Late() {
+    localStorage.setItem('Trumpet 3 Member 1 Late ', today);
+}
+
+function saveTrum3Mem2Late() {
+    localStorage.setItem('Trumpet 3 Member 2 Late ', today);
+}
+
+function saveTrum3Mem3Late() {
+    localStorage.setItem('Trumpet 3 Member 3 Late ', today);
+}
+
+function saveTrum3Mem4Late() { 
+    localStorage.setItem('Trumpet 3 Member 4 Late ', today);
+}
+
+function saveTrum3Mem5Late() {
+    localStorage.setItem('Trumpet 3 Member 5 Late ', today);
+}
+
+function saveTrum3Mem6Late() {
+    localStorage.setItem('Trumpet 3 Member 6 Late ', today);
+}
+
+function saveTrum3Mem7Late() {
+    localStorage.setItem('Trumpet 3 Member 7 Late ', today);
+}
+
+function saveTrum3Mem8Late() {
+    localStorage.setItem('Trumpet 3 Member 8 Late ', today);
+}
+
+function saveTrum3Mem9Late() {
+    localStorage.setItem('Trumpet 3 Member 9 Late ', today);
+}
+
+function saveTrum3Mem10Late() {
+    localStorage.setItem('Trumpet 3 Member 10 Late ', today);
 }
 
 // clear
@@ -293,15 +573,15 @@ function ClearAll() {
 function showAll() {
   if (CheckBrowser()) {
     var key = "";
-    var list = "<tr><th>Name</th>\n \t<th>Attendance</th></tr>";
+    var list = "<tr><th>Name</th>\n\n<th>Attendance</th></tr>";
     var i = 0;
     for (i = 0; i <= localStorage.length - 1; i++) {
       key = localStorage.key(i);
       list += "<tr><td>" + key + "</td>\n<td>"
           + localStorage.getItem(key) + "</td></tr>\n";
     }
-    if (list == "<tr><th>Name</th>\n<th>Attendance</th></tr>\n") {
-      list += "<tr><td><i>empty</i></td>\n<td><i>empty</i></td></tr>";
+    if (list == "<tr><th>Name</th>\n\n<th>Attendance</th></tr>") {
+      list += "<tr><td><i>empty</i></td>\n\n<td><i>empty</i></td></tr>";
     }
     document.getElementById('atlist').innerHTML = list;
   } else {
